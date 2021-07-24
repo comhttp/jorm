9c5fead1de6024ef13e05cfe8ac42c6b6462112e
marcetin <marcetin@gmail.com>
2021-07-24 14:36:16 +0200

    New Exchanges api

--- app/tickers.go
+++ app/tickers.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"fmt"
-	csrc "github.com/comhttp/jorm/mod/coins/src"
 	xsrc "github.com/comhttp/jorm/mod/exchanges/src"
 	"github.com/comhttp/jorm/mod/nodes"
 	//"github.com/comhttp/jorm/app/jorm/coin"
@@ -24,8 +23,6 @@ func (j *JORM) Tickers() {
 	//n.GetBitNodes(coins)
 	//go csrc.GetCoinSources()
 	go xsrc.GetExchangeSources(j.JDB)
-
-	csrc.GetCoinSources(j.JDB)
 
 	// dsrc.GetDataSources()
 }
