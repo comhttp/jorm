ef125dd4efe0a718be8d2eb1f663710789496af2
marcetin <marcetin@gmail.com>
2021-10-05 05:06:04 +0200

    as

--- app/jorm.go
+++ app/jorm.go
@@ -119,7 +119,7 @@ func (j *JORM) ENSOhandlers() http.Handler {
 	coin.ENSOroutes(cq, r)
 	exchange.ENSOroutes(eq, r)
 	explorer.ENSOroutes(exq, r)
-	return handlers.CORS()(handlers.CompressHandler(utl.InterceptHandler(utl.CommonMiddleware(r), utl.DefaultErrorHandler)))
+	return handlers.CORS()(handlers.CompressHandler(utl.InterceptHandler(r, utl.DefaultErrorHandler)))
 }
 
 func NewJORM(service, path, singleCoin string) (j *JORM) {
