72be55bbe1974de482e2607a9831ecf1eb9393f2
marcetin <marcetin@gmail.com>
2021-10-05 05:15:14 +0200

    dss

--- app/proxy.go
+++ app/proxy.go
@@ -37,7 +37,7 @@ func (h *baseHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		reverseproxy(w, r, "http://localhost:14444")
 	}
 	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
-	w.Write([]byte("403: Host forbidden " + host))
+	// w.Write([]byte("403: Host forbidden " + host))
 }
 
 func reverseproxy(w http.ResponseWriter, r *http.Request, target string) {
