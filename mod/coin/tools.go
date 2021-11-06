f2585b04590020520600ab3e54ef9d4104063037
marcetin <marcetin@gmail.com>
2021-11-06 03:58:59 +0100

    goo

--- mod/coin/tools.go
+++ mod/coin/tools.go
@@ -45,11 +45,8 @@ func (cq *CoinsQueries) ProcessCoins(coins []map[string]interface{}) {
 	coinsBin := Coins{N: 0}
 	allCoins := Coins{N: 0}
 	for i, c := range coins {
-		log.Print("pree Tu jeee")
 
 		cq.WriteCoin(c["slug"].(string), c)
-
-		log.Print("Tu jeee")
 
 		if c["algo"].(string) != "" &&
 			c["algo"].(string) != "N/A" &&
